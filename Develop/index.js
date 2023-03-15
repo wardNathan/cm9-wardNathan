21d5d95464420d5ada84adb29ba3d1a0fccc9179
wardNathan <xuserx98@gmail.com>
2023-03-15 13:36:02 -0600

    fixed error on question arrray

--- Develop/index.js
+++ Develop/index.js
@@ -1,69 +1,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [
-    {
-        type: 'input',
-        name: 'project',
-        message: 'Enter a title for your project:',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter what installations are necessary:'
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Enter the license that your project uses:',
-        choices: ['', 'Apache', 'BSD3', 'Creative Commons Attribution 4.0 International', 'Eclipse', 'IBM', 'MIT', 'Mozilla', 'Open Data Commons Attribution License (BY)', 'Open Data Commons Open Database License (ODbL)', 'Open Data Commons Public Domain Dedication and License (PDDL)', 'Perl The Perl License', 'Perl The Artistic License 2.0', 'Zlib'],
-    },
-    {
-        type: 'input',
-        name: 'Credits',
-        message: 'Enter in any contributions to the project:'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter in the steps to operate the project:'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter any tests that were ran on the project and their outcome:',
-    },
-    {
-        type: 'input',
-        name: 'githubUsername',
-        message: 'Enter your Github user name:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address',
-    }
-];
-
-function writeToFile(fileName, data) {}
+const questions = () =>{ 
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'Enter your project title',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Enter a description of your project',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Enter any necessary installations for the project',
+        },
+        {
+            type: 'checkbox',
+            name: 'License',
+            message: 'Enter the license that your project uses',
+            choices: ['', 'Apache', 'BSD3', 'Creative Commons Attribution 4.0 International', 'Eclipse', 'IBM', 'MIT', 'Mozilla', 'Open Data Commons Attribution License (BY)', 'Open Data Commons Open Database License (ODbL)', 'Open Data Commons Public Domain Dedication and License (PDDL)', 'Perl The Perl License', 'Perl The Artistic License 2.0', 'Zlib',],
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            message: 'Enter in any contributers to the project',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Enter in the steps to operate the project',
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'Enter in the tests that were run and their results',
+        },
+        {
+            type: 'input',
+            name: 'Github',
+            message: 'Enter your Github username',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'Enter your email',
+        },
+    ]);
+};
 
 function init() {
     questions()
     .then((data) => {
         console.log(data);
         return fs.writeFileSync("./README.md", generateMarkdown(data));
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         if (err) {
-            throw err;
+          throw err;
         }
-    });
+      });
 }
 
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,66 +1,64 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
-  if (license == 'None'){
+  if (License == 'None'){
     return '';
   }   
-  else if (license == 'Apache') {
+  else if (License == 'Apache') {
       return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
     } 
-  else if (license == 'BSD3') {
+  else if (License == 'BSD3') {
       return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
     } 
-  else if (license == 'BSD2') {
+  else if (License == 'BSD2') {
       return '[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
     }  
-  else if (license == 'Creative Commons Attribution 4.0 International') {
+  else if (License == 'Creative Commons Attribution 4.0 International') {
       return '[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)';
     } 
-  else if (license == 'Eclipse') {
+  else if (License == 'Eclipse') {
       return '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
     }    
-  else if (license == 'IBM') {
+  else if (License == 'IBM') {
       return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
     }
-  else if (license == 'MIT'){
+  else if (License == 'MIT'){
       return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
     }  
-  else if (license == 'Mozilla') {
+  else if (License == 'Mozilla') {
       return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
     }  
-  else if (license == 'Open Data Commons Attribution License (BY)') {
+  else if (License == 'Open Data Commons Attribution License (BY)') {
       return '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)';
     }  
-  else if (license == 'Open Data Commons Open Database License (ODbL)') {
+  else if (License == 'Open Data Commons Open Database License (ODbL)') {
       return '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)';
     }  
-  else if (license == 'Open Data Commons Public Domain Dedication and License (PDDL)') {
+  else if (License == 'Open Data Commons Public Domain Dedication and License (PDDL)') {
       return '[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)';
     }  
-  else if (license == 'Perl The Perl License') {
+  else if (License == 'Perl The Perl License') {
       return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
     }  
-  else if (license == 'Perl The Artistic License 2.0') {
+  else if (License == 'Perl The Artistic License 2.0') {
       return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
     }  
-  else if (license == 'Zlib') {
+  else if (License == 'Zlib') {
       return '[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)';
     }  
   }
   ;
   
-  // TODO: Create a function to generate markdown for README
+
   function generateMarkdown(data) {
     return `
 
-    # ${data.projectName}
+    # ${data.Title}
 
     ## License
-    ${renderLicenseBadge(data.license)}
+    ${renderLicenseBadge(data.License)}
 
     ## Description
-    ${data.description}
+    ${data.Description}
 
     ## Table of Contents
     - [License](#License)
@@ -72,22 +70,22 @@ function renderLicenseBadge(license) {
     - [Questions](#Questions)
     
     ## Installation
-    ${data.installation}
+    ${data.Installation}
 
     ## Usage
     ![alttext](./assets/images/{add name of picture here})
-    ${data.usage}
+    ${data.Usage}
     
     ## Credits
-    ${data.credits}
+    ${data.Credits}
 
     ## Tests
-    ${data.tests}
+    ${data.Tests}
     
     ## Questions
     If you would like to ask me any questions. Contact me at my email address below or you can check out my Github profile.
-    - Email-${data.email}
-    - Github user name- ${data.githubUsername}
+    - Github user name- ${data.Github}
+    - Email-${data.Email}
     
     
   `;
